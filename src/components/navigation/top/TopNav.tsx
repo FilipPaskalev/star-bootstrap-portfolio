2284132371cb4256bc91977e2e3ad2b05c07af3f
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-26 23:17:00 +0100

    refactor: add const for label for easier reading

--- src/components/navigation/top/TopNav.tsx
+++ src/components/navigation/top/TopNav.tsx
@@ -7,23 +7,31 @@ import "./top-nav.css";
 
 const TopNav = () => {
   const brandLabel: string = "Brand";
+  const homeLinkLabel: string = "Home";
+  const contactLinkLabel: string = "Contact";
+  const resumeLinkLabel: string = "Resume";
+  const projectsLinkLabel: string = "Projects";
 
   return (
-    <Navbar className="top-nav-container">
+    <Navbar className="top-nav-bar">
       <Container fluid>
         <NavBrand label={brandLabel} />
         <Nav className="ms-auto">
           <Nav.Link as={Link} to={EPagesPaths.HOME} className="nav-link">
-            <FaHome className="nav-icon" /> Home
+            <FaHome className="nav-icon" />
+            {homeLinkLabel}
           </Nav.Link>
           <Nav.Link as={Link} to={EPagesPaths.CONTACT} className="nav-link">
-            <FaEnvelope className="nav-icon" /> Contact
+            <FaEnvelope className="nav-icon" />
+            {contactLinkLabel}
           </Nav.Link>
           <Nav.Link as={Link} to={EPagesPaths.RESUME} className="nav-link">
-            <FaFileAlt className="nav-icon" /> Resume
+            <FaFileAlt className="nav-icon" />
+            {resumeLinkLabel}
           </Nav.Link>
           <Nav.Link as={Link} to={EPagesPaths.PROJECTS} className="nav-link">
-            <FaProjectDiagram className="nav-icon" /> Projects
+            <FaProjectDiagram className="nav-icon" />
+            {projectsLinkLabel}
           </Nav.Link>
         </Nav>
       </Container>
