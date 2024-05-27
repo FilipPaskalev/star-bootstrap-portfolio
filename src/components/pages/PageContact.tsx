60124240a999dd30a40779548e99e0f0f8ce46b4
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-27 01:37:55 +0100

    refactor

--- src/components/pages/PageContact.tsx
+++ src/components/pages/PageContact.tsx
@@ -1,13 +1,8 @@
 // src/pages/Contact.tsx
 import React from "react";
-import PageContent from "../templates/PageContent";
 
 const Contact: React.FC = () => {
-  return (
-    <PageContent>
-      <h1>Contact Page</h1>
-    </PageContent>
-  );
+  return <h1>Contact Page</h1>;
 };
 
 export default Contact;
