b087cf324287cb874d69b57b86a55b91a27e0986
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-27 02:11:01 +0100

    add comments + remove unused prop

--- src/components/navigation/brand/NavBrand.tsx
+++ src/components/navigation/brand/NavBrand.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
-import { Navbar, Container, Modal, Button } from "react-bootstrap";
-import { FaMoon, FaSun, FaLanguage } from "react-icons/fa";
+// React-Bootstrap
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+// Styles
 import "./nav-brand.css";
+// i18n translation
 import { useTranslation } from "react-i18next";
+// Icons
 import { US, BG } from "country-flag-icons/react/3x2";
+import { FaMoon, FaSun, FaLanguage } from "react-icons/fa";
 
 type NavBrandProps = {
-  showIcons?: boolean;
   children?: React.ReactNode;
 };
 
-const NavBrand = ({ showIcons = true, children }: NavBrandProps) => {
+const NavBrand = ({ children }: NavBrandProps) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const { t, i18n } = useTranslation();
@@ -31,16 +37,14 @@ const NavBrand = ({ showIcons = true, children }: NavBrandProps) => {
 
   return (
     <Navbar.Brand className="nav-brand-container">
-      {showIcons && (
-        <Container className="action-icons">
-          <span onClick={toggleTheme} className="theme-icon">
-            {isDarkTheme ? <FaSun /> : <FaMoon />}
-          </span>
-          <span onClick={toggleModal} className="language-icon">
-            <FaLanguage />
-          </span>
-        </Container>
-      )}
+      <Container className="action-icons">
+        <span onClick={toggleTheme} className="theme-icon">
+          {isDarkTheme ? <FaSun /> : <FaMoon />}
+        </span>
+        <span onClick={toggleModal} className="language-icon">
+          <FaLanguage />
+        </span>
+      </Container>
       {children}
       <Modal show={showModal} onHide={toggleModal}>
         <Modal.Header closeButton>
