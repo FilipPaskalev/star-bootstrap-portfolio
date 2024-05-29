31fb493b93b9fac520e83712ad321666e91febaf
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-29 09:58:58 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import PageHome from "./components/pages/PageHome";
 import EPagesPaths from "./enum/EPagesPaths";
-import Navigation from "./components/navigation/Navigation";
-import getWindowSize from "./utils/getWindowsSize";
-import WrapperPageContent from "./components/wrappers/pageContent/WrapperPageContent";
 import "./app.css";
 
 const App = () => {
-  const [windowSize, setWindowSize] = useState(getWindowSize());
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize(getWindowSize());
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <>
-      <Navigation width={windowSize.width} />
-      <WrapperPageContent>
-        <Routes>
-          <Route path={EPagesPaths.HOME} element={<PageHome />} />
-        </Routes>
-      </WrapperPageContent>
-    </>
+    <Routes>
+      <Route path={EPagesPaths.HOME} element={<PageHome />} />
+    </Routes>
   );
 };
 
--- src/utils/scrollToAnchor.tsx
+++
@@ -1,1 +0,0 @@
-// TODO: TASK -> function takes like parameter page path + anchor id and scrolls to that anchor
