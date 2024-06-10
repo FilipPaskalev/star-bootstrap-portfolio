acea31eb023b4ed3efeb8be54eb4d18e5a491b7d
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-06-10 15:15:26 +0100

    add badges for tech stack

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,12 +1,15 @@
 import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import ListGroup from "react-bootstrap/ListGroup";
 
 type Props = {
   title: string;
   description: string;
   imgSrc: string;
+  technologies: string[];
 };
 
-const ProjectCard = ({ title, description, imgSrc }: Props) => {
+const ProjectCard = ({ title, description, imgSrc, technologies }: Props) => {
   return (
     <Card>
       <Card.Img variant="top" src={imgSrc} />
@@ -14,6 +17,18 @@ const ProjectCard = ({ title, description, imgSrc }: Props) => {
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item className="text-center">
+          <strong>Used technologies</strong>
+          <div>
+            {technologies.map((tech, index) => (
+              <Badge key={index} pill className="mx-1">
+                {tech}
+              </Badge>
+            ))}
+          </div>
+        </ListGroup.Item>
+      </ListGroup>
     </Card>
   );
 };
