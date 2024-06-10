4a91c6a01a26ae562e51f307924ed45e91bf593e
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-06-10 14:47:16 +0100

    add layouts for projects

--- src/pages/myWork/MyWorkPage.tsx
+++ src/pages/myWork/MyWorkPage.tsx
@@ -1,12 +1,32 @@
 import ProjectsCarousel from "../../components/carousel/ProjectsCarousel";
-import useBreakpoint from "../../hooks/useBreakpoint";
-import MainContentWrapper from "../../wrappers/mainContent/MainContentWrapper";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import "./my-work-page.scss";
 
 const MyWorkPage = () => {
-  const breakpoint = useBreakpoint();
-
-  return <ProjectsCarousel />;
+  return (
+    <>
+      <ProjectsCarousel />
+      <Row xs={1} md={2} lg={3} xxl={4} className="g-4">
+        {Array.from({ length: 9 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Img variant="top" src="https://placehold.co/120x60" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default MyWorkPage;
