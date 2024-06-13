833e72b54c2c7b0c466d0624ac979bd4a4005faa
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-06-13 17:31:02 +0100

    add form structure

--- src/pages/letsTalk/LetsTalkPage.tsx
+++ src/pages/letsTalk/LetsTalkPage.tsx
@@ -7,7 +7,40 @@ const LetsTalkPage = () => {
 
   return (
     <MainContentWrapper breakpoint={breakpoint}>
-      Lets talk page
+      <form className="form">
+        <div className="flex">
+          <label>
+            <input className="input" type="text" placeholder="" />
+            <span>first name</span>
+          </label>
+
+          <label>
+            <input className="input" type="text" placeholder="" />
+            <span>last name</span>
+          </label>
+        </div>
+
+        <label>
+          <input className="input" type="email" placeholder="" />
+          <span>email</span>
+        </label>
+
+        <label>
+          <input className="input" placeholder="" type="tel" />
+          <span>contact number</span>
+        </label>
+        <label>
+          <textarea className="input01" placeholder=""></textarea>
+          <span>message</span>
+        </label>
+
+        <button className="fancy">
+          <span className="top-key"></span>
+          <span className="text">submit</span>
+          <span className="bottom-key-1"></span>
+          <span className="bottom-key-2"></span>
+        </button>
+      </form>
     </MainContentWrapper>
   );
 };
